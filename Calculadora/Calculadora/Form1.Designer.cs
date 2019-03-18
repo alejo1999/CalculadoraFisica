7449ebaef4ce03136e5ac0fd90c202e983c647b2
alejo1999 <alenetoo1999@gmail.com>
2019-03-17 19:59:28 -0600

    primeros avances diseño

---
+++ Calculadora/Calculadora/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.sombra_izquierda = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(41)))), ((int)(((byte)(53)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1102, 5);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.panel2.Controls.Add(this.sombra_izquierda);
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(0, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(243, 614);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(41)))), ((int)(((byte)(53)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 5);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1102, 19);
+            this.panel3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(10, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 56);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "T. de la Relatividad";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(10, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(230, 56);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Ley de gravitación Universal de Newton";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(10, 195);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(230, 56);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Teorema de Pitágoras";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(10, 257);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(230, 56);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Dilatacion del tiempo por velocidad";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(10, 319);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(230, 56);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Primera Ley de Newton";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(10, 381);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(230, 56);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Segunda Ley de Newton";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(3, 584);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(242, 30);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "Integrantes";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // sombra_izquierda
+            // 
+            this.sombra_izquierda.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(186)))), ((int)(((byte)(239)))));
+            this.sombra_izquierda.Location = new System.Drawing.Point(2, 71);
+            this.sombra_izquierda.Name = "sombra_izquierda";
+            this.sombra_izquierda.Size = new System.Drawing.Size(8, 56);
+            this.sombra_izquierda.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 638);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel sombra_izquierda;
+    }
+}
+
---
+++ Calculadora/Calculadora/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            sombra_izquierda.Height = button1.Height;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sombra_izquierda.Height = button1.Height;
+            sombra_izquierda.Top = button1.Top;
+            button1.BackColor = Color.FromArgb(1,68,87);
+            button2.BackColor = Color.FromArgb(11, 32,39);
+            button3.BackColor = Color.FromArgb(11, 32,39);
+            button4.BackColor = Color.FromArgb(11, 32,39);
+            button5.BackColor = Color.FromArgb(11, 32,39);
+            button6.BackColor = Color.FromArgb(11, 32,39);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            sombra_izquierda.Height = button2.Height;
+            sombra_izquierda.Top = button2.Top;
+            
+            button1.BackColor = Color.FromArgb(11, 32,39);
+            button2.BackColor = Color.FromArgb(1, 68, 87);
+            button3.BackColor = Color.FromArgb(11, 32,39);
+            button4.BackColor = Color.FromArgb(11, 32,39);
+            button5.BackColor = Color.FromArgb(11, 32,39);
+            button6.BackColor = Color.FromArgb(11, 32,39);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sombra_izquierda.Height = button3.Height;
+            sombra_izquierda.Top = button3.Top;
+            button1.BackColor = Color.FromArgb(11, 32,39);
+            button2.BackColor = Color.FromArgb(11, 32,39);
+            button3.BackColor = Color.FromArgb(1, 68, 87);
+            button4.BackColor = Color.FromArgb(11, 32,39);
+            button5.BackColor = Color.FromArgb(11, 32,39);
+            button6.BackColor = Color.FromArgb(11, 32,39);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sombra_izquierda.Height = button4.Height;
+            sombra_izquierda.Top = button4.Top;
+
+            button1.BackColor = Color.FromArgb(11, 32,39);
+            button2.BackColor = Color.FromArgb(11, 32,39);
+            button3.BackColor = Color.FromArgb(11, 32,39);
+            button4.BackColor = Color.FromArgb(1, 68, 87);
+            button5.BackColor = Color.FromArgb(11, 32,39);
+            button6.BackColor = Color.FromArgb(11, 32,39);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sombra_izquierda.Height = button5.Height;
+            sombra_izquierda.Top = button5.Top;
+            
+            button1.BackColor = Color.FromArgb(11, 32,39);
+            button2.BackColor = Color.FromArgb(11, 32,39);
+            button3.BackColor = Color.FromArgb(11, 32,39);
+            button4.BackColor = Color.FromArgb(11, 32,39);
+            button5.BackColor = Color.FromArgb(1, 68, 87);
+            button6.BackColor = Color.FromArgb(11, 32,39);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            sombra_izquierda.Height = button6.Height;
+            sombra_izquierda.Top = button6.Top;
+
+            button1.BackColor = Color.FromArgb(11, 32,39);
+            button2.BackColor = Color.FromArgb(11, 32,39);
+            button3.BackColor = Color.FromArgb(11, 32,39);
+            button4.BackColor = Color.FromArgb(11, 32,39);
+            button5.BackColor = Color.FromArgb(11, 32,39);
+            button6.BackColor = Color.FromArgb(1, 68, 87);
+        }
+    }
+}
